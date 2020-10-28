fd079f5237412ba1d83c039a53561b2afddeaa0e
vvEii <coopershang125@gmail.com>
2020-10-27 17:34:42 -0700

    refactor InterviewerListItem component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import classNames from 'classnames';
+import 'components/InterviewerListItem.scss';
 
 const InterviewerListItem = (props) => {
+  const interviewerItemClass = classNames('interviews__item', {
+    'interviewers__item--selected': props.selected,
+  });
+
+  const itemImageClass = classNames('interviewers__item-image', {
+    'interviewers__item-image--selected': props.selected,
+  });
+
   return (
-    <li className='interviewers__item'>
-      <img
-        className='interviewers__item-image'
-        src='https://i.imgur.com/LpaY82x.png'
-        alt='Sylvia Palmer'
-      />
-      Sylvia Palmer
+    <li
+      className={interviewerItemClass}
+      onClick={() => {
+        props.setInterviewer(props.name);
+      }}
+    >
+      <img className={itemImageClass} src={props.avatar} alt={props.name} />
+      {props.selected ? props.name : ''}
     </li>
   );
 };
