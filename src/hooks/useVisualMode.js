668e5ea147361a7c2513037ea147a25c9e9cbe60
vvEii <coopershang125@gmail.com>
2020-10-29 17:00:49 -0700

    add useVisualMode hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (mode, replace = false) => {
+    if (!replace) {
+      setHistory([...history, mode]);
+      setMode(mode);
+    } else {
+      setMode(mode);
+    }
+  };
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    } else {
+      setMode(initial);
+    }
+  };
+  return {
+    mode,
+    transition,
+    back,
+  };
+}
