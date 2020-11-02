f0604bcb5d4bc99355c1767cd32182bb60209493
vvEii <coopershang125@gmail.com>
2020-11-02 15:15:30 -0800

    add runtime props type check

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import InterviewerListItem from './InterviewerListItem';
-import 'components/InterviewerList.scss'
+import 'components/InterviewerList.scss';
+import PropTypes from 'prop-types';
 
 const InterviewerList = (props) => {
-
   return (
     <section className='interviewers'>
       <h4 className='interviewers__header text--light'>Interviewer</h4>
       <ul className='interviewers__list'>
         {props.interviewers.map((interviewer) => (
-          <InterviewerListItem 
-          name = {interviewer.name}
-          avatar = {interviewer.avatar}
-          selected = {interviewer.id === props.interviewer}
-          setInterviewer = {event => props.setInterviewer(interviewer.id)}
-          key = {interviewer.id}
+          <InterviewerListItem
+            name={interviewer.name}
+            avatar={interviewer.avatar}
+            selected={interviewer.id === props.interviewer}
+            setInterviewer={(event) => props.setInterviewer(interviewer.id)}
+            key={interviewer.id}
           />
         ))}
       </ul>
     </section>
   );
 };
+// check interviewers type runtime
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
+
 export default InterviewerList;
